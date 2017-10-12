bf59e9248d3bf3ce78eed6e517e85467db565a09
Wentao Hao <wthao@mobvoi.com>
2017-10-12 18:07:29 +0800

    修复 amazon.com 商品页面的评分选择器问题

--- app/scripts/track/amazon.com.product.js
+++ app/scripts/track/amazon.com.product.js
@@ -25,7 +25,7 @@ function crawl(){
   data.review_count = document.querySelector('.totalReviewCount').innerText.replace(',','');
   data.rating = document.querySelector('.arp-rating-out-of-text').innerText.replace(' out of 5 stars','');
   data.price = document.querySelector('.a-spacing-none .a-color-price').innerText;
-  var per = document.querySelectorAll('.a-text-right a.histogram-review-count');
+  var per = document.querySelectorAll('#histogramTable .a-text-right .a-size-base');
   data.percentage = {
     '5': per[0].innerText, 
     '4': per[1].innerText,
@@ -33,11 +33,12 @@ function crawl(){
     '2': per[3].innerText, 
     '1': per[4].innerText
   };
-  data.salesrank = document.querySelector('#SalesRank').innerText;
+  data.salesrank = (document.querySelector('#SalesRank'))?document.querySelector('#SalesRank').innerText:'';
   data.category = document.querySelector('#wayfinding-breadcrumbs_feature_div').innerText;
   console.log(data);
 
   chrome.runtime.sendMessage({ 'msgtype': 'amazon.com.product', 'content': data}, function (response) {
+  //chrome.runtime.sendMessage({ 'msgtype': 'test', 'content': data}, function (response) {
     //console.log(response);
   });
 
