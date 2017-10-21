8f4533d9c6f34fa4e11544047475a2e740099a04
Wentao <icewent@gmail.com>
2017-10-21 20:11:38 +0800

    在帮助文档中添加数据源列表

---
+++ app/scripts/help.js
@@ -0,0 +1,9 @@
+'use strict';
+
+$(function(){
+
+  for(var i in config){
+    var li = '<li><a href="/mydata.html?source=' + i + '">' + config[i].display_name + '</a></li>';
+    document.querySelector('#source-list').innerHTML += li;
+  }
+});
